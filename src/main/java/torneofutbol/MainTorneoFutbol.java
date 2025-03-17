4edc710ce61ff375c00ba8b49a98ab3a74b346d7
Pablogg25 <pgonzalezg12@educantabria.es>
2025-03-17 15:55:50 +0100

    Cambio de mensaje de entrada 2
    Cambio el mensaje de entrada de nuevo

--- src/main/java/torneofutbol/MainTorneoFutbol.java
+++ src/main/java/torneofutbol/MainTorneoFutbol.java
@@ -5,7 +5,7 @@ public class MainTorneoFutbol {
 
     public static void main(String[] args) {
 
-        System.out.println("Ejercicio 1:");
+        System.out.println("Mostrando ejercicio 1 Pablo");
         
         // Crear una instancia de la clase Equipo
         Equipo equipoMH = new Equipo("IES MH FC");
